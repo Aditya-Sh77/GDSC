a780ca6581f697726a91722ffbe34bddd731e240
Aditya-Sh77 <148966260+Aditya-Sh77@users.noreply.github.com>
2024-12-14 01:11:36 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -63,7 +63,7 @@ y = data['G3']  # Final grade
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train Decision Tree Regressor
-model = DecisionTreeRegressor(random_state=42)
+model = DecisionTreeRegressor(max_depth=10, min_samples_split=3, min_samples_leaf=2, max_features='sqrt', random_state=42)
 model.fit(X_train, y_train)
 
 # Evaluate
